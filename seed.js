0d832be78eff3036f4c0d2810ecf5f81d9a9d8bb
Viktor Plane <viktor.plane@chromaway.com>
2021-12-03 23:39:23 +0100

    Remove description

--- seed.js
+++ seed.js
@@ -20,8 +20,6 @@ function main() {
         : 'Chromia Mysterious Seed',
       image:
         'https://public.nftstatic.com/static/nft/res/ba97a491604b45ac97093ae16658701a.jpeg',
-      description:
-        'This exquisite pouch emits colorful lights. There has to be something grand inside! Redeem it in the upcoming companion app for My Neighbor Alice.',
     });
   }
 
